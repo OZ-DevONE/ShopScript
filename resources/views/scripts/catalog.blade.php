44a344de26095961058c1e4251cc4ae906d2b71a
RGRG42F <kurbanov.radmir@list.ru>
2024-02-21 08:27:15 +0500

    Изменил страницу cataog.blade.php

--- resources/views/scripts/catalog.blade.php
+++ resources/views/scripts/catalog.blade.php
@@ -4,24 +4,37 @@
 
 @section('body')
 
-    @if ($scripts->count() > 0)
-        @foreach ($scripts as $scripts_item)
-            <div class="card" style="width: 18rem;">
-                <img src="/storage/scripts/{{$scripts_item->image}}" class="card-img-top" alt="{{$scripts_item->image}}">
-                <div class="card-body">
-                    <h5 class="card-title">{{$scripts_item->title}}</h5>
-                    <p class="card-text"> {{$scripts_item->description}} </p>
-                    <h6 class="card-title"> {{$scripts_item->price}} ₽ </h6>
-                    <a href="#" class="btn btn-primary">Подробнее</a>
-                </div>
-            </div>
-        @endforeach
+    <div class="container">
 
-        <div class="mt-4">
-            {{ $scripts->links() }}
+        <h2 class="mt-5">
+            Каталог
+        </h2>
+
+        <div class="mt-3">
+
+            @if ($scripts->count() > 0)
+                @foreach ($scripts as $scripts_item)
+                    <div class="card" style="width: 18rem;">
+                        <img src="/storage/scripts/{{ $scripts_item->image }}" class="card-img-top"
+                            alt="{{ $scripts_item->image }}">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $scripts_item->title }}</h5>
+                            <p class="card-text"> {{ $scripts_item->description }} </p>
+                            <h6 class="card-title"> {{ $scripts_item->price }} ₽ </h6>
+                            <a href="#" class="btn btn-primary">Подробнее</a>
+                        </div>
+                    </div>
+                @endforeach
+
+                <div class="mt-4">
+                    {{ $scripts->links() }}
+                </div>
+            @else
+                <p>Товаров не найдено.</p>
+            @endif
+
         </div>
-    @else
-        <p>Товаров не найдено.</p>
-    @endif
+
+    </div>
 
 @endsection
